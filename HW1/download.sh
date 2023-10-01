b997b199115e9e7c1986b95beb175eb6e7001e62
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-10-02 03:00:57 +0800

    Add files via upload

---
+++ HW1/download.sh
@@ -0,0 +1,5 @@
+mkdir ./multiple-choice/model
+
+gdown 1e9ggtDDt6Ri6hnsU5CSbBPH7ux3tIQoh -O ./multiple-choice/model/trained_model.zip
+
+unzip ./multiple-choice/model/trained_model.zip -d ./multiple-choice/model
