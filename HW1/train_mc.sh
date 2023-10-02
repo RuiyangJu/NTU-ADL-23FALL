38aa78c36e07e74338e54282583867378e549362
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-10-03 01:29:21 +0800

    Add files via upload

---
+++ HW1/train_mc.sh
@@ -0,0 +1,11 @@
+python ./multiple-choice/train_mc.py \
+  --model_name_or_path hfl/chinese-macbert-large \
+  --train_file ./data/train.json \
+  --validation_file ./data/valid.json \
+  --context_file ./data/context.json \
+  --max_length 512 \
+  --per_device_train_batch_size 4 \
+  --gradient_accumulation_steps 16 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 10 \
+  --output_dir ./multiple-choice/model
