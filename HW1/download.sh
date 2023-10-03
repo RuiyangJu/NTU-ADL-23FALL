a06f645a3902ae0921c6c0202dddd86db5d981ab
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-10-03 20:34:11 +0800

    Update download.sh

--- HW1/download.sh
+++ HW1/download.sh
@@ -5,5 +5,5 @@ gdown 1Pgbvr3CYRUT8fSwmjszdPdqu_C73vCu0 -O ./data/all_data.zip
 gdown 1e9ggtDDt6Ri6hnsU5CSbBPH7ux3tIQoh -O ./multiple-choice/model/trained_model.zip
 
 
-unzip ../data/all_data.zip -d ./data
+unzip ./data/all_data.zip -d ./data
 unzip ./multiple-choice/model/trained_model.zip -d ./multiple-choice/model
