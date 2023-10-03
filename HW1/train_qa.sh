7193f240046680ff6714a00d85fa9685c68c4e03
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-10-03 18:26:06 +0800

    Add files via upload

---
+++ HW1/train_qa.sh
@@ -0,0 +1,11 @@
+python ./question-answering/train_qa.py \
+  --model_name_or_path hfl/chinese-lert-large \
+  --train_file ./data/train.json \
+  --validation_file ./data/valid.json \
+  --context_file ./data/context.json \
+  --max_seq_length 512 \
+  --per_device_train_batch_size 4 \
+  --gradient_accumulation_steps 16 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 10 \
+  --output_dir ./question-answering/model
