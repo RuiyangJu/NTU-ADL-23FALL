59183bdb97742e519810860b55519f6970c96160
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-10-04 23:38:15 +0800

    Update run.sh

--- HW1/run.sh
+++ HW1/run.sh
@@ -6,8 +6,8 @@ python ./multiple-choice/test_mc.py \
   --model_name_or_path ./multiple-choice/model \
   --config_name ./multiple-choice/model \
   --tokenizer_name ./multiple-choice/model \
-  --test_file ${2} \
   --context_file ${1} \
+  --test_file ${2} \
   --max_length 512 \
   --per_device_test_batch_size 4 \
   --output_dir ./multiple-choice/output
@@ -16,8 +16,8 @@ python ./question-answering/test_qa.py \
   --model_name_or_path ./question-answering/model \
   --config_name ./question-answering/model \
   --tokenizer_name ./question-answering/model \
-  --test_file ${2} \
   --context_file ${1} \
+  --test_file ${2} \
   --predict_mc_file ./multiple-choice/output/predict.json \
   --preprocessing_num_workers 12 \
   --max_length 512 \
