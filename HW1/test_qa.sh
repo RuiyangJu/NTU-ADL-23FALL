ebc8efe77ef62a851a97342fa7cf6cf56bd3359e
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-10-04 22:19:05 +0800

    Update test_qa.sh

--- HW1/test_qa.sh
+++ HW1/test_qa.sh
@@ -1,13 +1,13 @@
 python ./question-answering/test_qa.py \
-    --model_name_or_path ./question-answering/model \
-    --config_name ./question-answering/model \
-    --tokenizer_name ./question-answering/model \
-    --test_file ./data/test.json \
-    --context_file ./data/context.json \
-    --predict_mc_file ./multiple-choice/output/predict.json \
-    --preprocessing_num_workers 12 \
-    --max_length 512 \
-    --doc_stride 128 \
-    --do_predict \
-    --per_device_eval_batch_size 1 \
-    --output_dir ./question-answering/output.csv
+  --model_name_or_path ./question-answering/model \
+  --config_name ./question-answering/model \
+  --tokenizer_name ./question-answering/model \
+  --test_file ./data/test.json \
+  --context_file ./data/context.json \
+  --predict_mc_file ./multiple-choice/output/predict.json \
+  --preprocessing_num_workers 12 \
+  --max_length 512 \
+  --doc_stride 128 \
+  --do_predict \
+  --per_device_eval_batch_size 1 \
+  --output_dir ./question-answering/output/output.csv
