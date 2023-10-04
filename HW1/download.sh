0b74d786beee3350ad3341e07ef2577c2dc8020b
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-10-04 22:00:26 +0800

    Update download.sh

--- HW1/download.sh
+++ HW1/download.sh
@@ -1,8 +1,11 @@
 mkdir ./data
 mkdir ./multiple-choice/model
+mkdir ./question-answering/model
 
 gdown 1Pgbvr3CYRUT8fSwmjszdPdqu_C73vCu0 -O ./data/all_data.zip
-gdown 1e9ggtDDt6Ri6hnsU5CSbBPH7ux3tIQoh -O ./multiple-choice/model/trained_model.zip
+gdown 1DSXtdsLhw8cp2J7yrQL41DAZX-EVDcuS -O ./multiple-choice/model/model_mc.zip
+gdown 1KChE3Jo_6ORK-SsJtUq3vGjSUyFCTDMU -O ./question-answering/model/model_qa.zip
 
 unzip ./data/all_data.zip -d ./data
-unzip ./multiple-choice/model/trained_model.zip -d ./multiple-choice/model
+unzip ./multiple-choice/model/model_mc.zip -d ./multiple-choice/model
+unzip ./question-answering/model/model_qa.zip -d ./question-answering/model
