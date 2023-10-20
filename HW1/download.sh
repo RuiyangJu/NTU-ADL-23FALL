9013c6049ff8f48c775f43995566c90ed06ab03e
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-10-21 01:23:49 +0800

    Update download.sh

--- HW1/download.sh
+++ HW1/download.sh
@@ -1,14 +1,9 @@
-mkdir ./data
-mkdir ./multiple-choice/output
-# mkdir ./multiple-choice/model
+mkdir ./multiple-choice/model
 mkdir ./question-answering/model
+mkdir ./multiple-choice/output
 
-gdown 1Pgbvr3CYRUT8fSwmjszdPdqu_C73vCu0 -O ./data/all_data.zip
-gdown 13clU4Y_7dl5nv8ae23Br1dRGo0X7DhcJ -O ./multiple-choice/output/predict_mc.zip
-# gdown 1DSXtdsLhw8cp2J7yrQL41DAZX-EVDcuS -O ./multiple-choice/model/model_mc.zip
+gdown 1DSXtdsLhw8cp2J7yrQL41DAZX-EVDcuS -O ./multiple-choice/model/model_mc.zip
 gdown 1KChE3Jo_6ORK-SsJtUq3vGjSUyFCTDMU -O ./question-answering/model/model_qa.zip
 
-unzip ./data/all_data.zip -d ./data
-unzip ./multiple-choice/output/predict_mc.zip -d ./multiple-choice/output
-# unzip ./multiple-choice/model/model_mc.zip -d ./multiple-choice/model
+unzip ./multiple-choice/model/model_mc.zip -d ./multiple-choice/model
 unzip ./question-answering/model/model_qa.zip -d ./question-answering/model
