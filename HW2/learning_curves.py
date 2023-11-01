c256baf21d595cf7dc643c000d71507bf283b6f4
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-11-01 13:28:36 +0800

    Update learning_curves.py

--- HW2/learning_curves.py
+++ HW2/learning_curves.py
@@ -29,7 +29,7 @@ y = [step['eval_rouge-l_p'] for step in log_history if 'eval_rouge-l_p' in step.
 plt.plot(x, y, label="rouge-l_p")
 y = [step['eval_rouge-l_r'] for step in log_history if 'eval_rouge-l_r' in step.keys()]
 plt.plot(x, y, label="rouge-l_r")
-plt.xlabel('Traing Steps')
+plt.xlabel('Training Steps')
 plt.ylabel('ROUGE')
 plt.legend(bbox_to_anchor=(1.05,1), loc='upper left')
 plt.tight_layout()
