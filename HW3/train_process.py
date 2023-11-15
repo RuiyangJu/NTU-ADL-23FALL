35fbe1b65b1b30e40faae27413eb906948590606
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-11-15 17:16:59 +0800

    Add files via upload

---
+++ HW3/train_process.py
@@ -0,0 +1,14 @@
+import json
+
+with open('data/train.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+for key in data:
+    if key["id"]:
+        del key["id"]
+    key["input"] = ""
+    key["output"] = ""
+print(data)
+
+with open('processed_data/processed_train.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
