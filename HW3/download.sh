97e3e101daff8efa726add4ba5371f675dab35d0
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-11-23 11:28:32 +0800

    Update download.sh

--- HW3/download.sh
+++ HW3/download.sh
@@ -1,5 +1,5 @@
 mkdir ./model
 
-gdown 1LyzAmzU80E756DR_eycCUer2WqzUh6vk -O ./model/hw3_model.zip
+gdown 1THat2vzdABH9It91oWXV62I2LfTPnRSY -O ./model/hw3_model.zip
 
 unzip ./model/hw3_model.zip -d ./model
